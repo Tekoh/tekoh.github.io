e1b697630ad6639012399fbf56e66220331b10eb
Mahd Raihan <l39583066@student.ua92.ac.uk>
2025-01-25 02:47:04 +0000

    Personal Portfolio Initial

---
+++ data/js/portfolio_main.js
@@ -0,0 +1,8 @@
+function NavMode__Mobile() {
+  var x = document.getElementById("MyNavBar-Container");
+  if (x.className === "NavBar-Container") {
+    x.className += " mobile";
+  } else {
+    x.className = "NavBar-Container";
+  }
+}
